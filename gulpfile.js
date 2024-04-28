09961efaa248e438f7c522f07e694d185f3297fd
Elis Antonio Perez <codewithleader@gmail.com>
2024-04-28 00:28:41 -0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,53 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const {
+  src,
+  dest,
+  watch,
+  series,
+  // parallel
+} = require('gulp');
+
+// Css + Sass
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
-function sassToCssCompiler(done) {
+// Images
+const imagemin = require('gulp-imagemin');
+
+function scssToCssCompiler(done) {
   // Pasos para compilar los archivos Sass
   // 1. Identificar el archivo Sass
-  src('src/scss/app.scss')
+  src('src/scss/style.scss')
     // 2. Compilarlo
     .pipe(sass())
     // .pipe(sass({ outputStyle: 'compressed' })) // Minificar el CSS
     // .pipe(sass({ outputStyle: 'expanded' })) // Expandir el CSS
     .pipe(postcss([autoprefixer()])) // Agregar los prefijos de compatibilidad
     // 3. Guardar el archivo css
-    .pipe(dest('build/css'));
+    .pipe(dest('public/css'));
 
   done();
 }
 
-function devWatch() {
-  watch('src/scss/**/*.scss', sassToCssCompiler);
+function optimizeImg(done) {
+  src('src/images/**/*')
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest('public/img'));
+
+  done();
 }
 
-exports.sassToCssCompiler = sassToCssCompiler;
-exports.devWatch = devWatch;
+function watchTask() {
+  watch('src/scss/**/*.scss', scssToCssCompiler);
+  watch('src/images/**/*', optimizeImg);
+}
 
-exports.default = series(sassToCssCompiler, devWatch);
+exports.scssToCssCompiler = scssToCssCompiler;
+exports.optimizeImg = optimizeImg;
+exports.watchTask = watchTask;
+
+exports.default = series(optimizeImg, scssToCssCompiler, watchTask);
+// exports.default = parallel(optimizeImg, scssToCssCompiler, watchTask);
 
 // series: Ejecuta tareas en serie
 // parallel: Ejecuta varias tareas en paralelo
